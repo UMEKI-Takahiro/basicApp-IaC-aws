bbb28bfaa4da678920080129944e9b27845a46e1
梅木崇浩 <forprogramming.b1@gmail.com>
2025-03-03 23:55:20 +0900

    create igw and public route table

--- network.tf
+++ network.tf
@@ -1,10 +1,28 @@
+######################
 # vpc
+######################
+
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "basicApp-vpc"
   }
 }
+
+######################
+# internet gateway
+######################
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "basicApp-igw"
+  }
+}
+
+######################
+# public subnet
+######################
 
 # public subnet
 resource "aws_subnet" "public_subnet" {
@@ -16,6 +34,28 @@ resource "aws_subnet" "public_subnet" {
     Name = "basicApp-public-subnet"
   }
 }
+
+# route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+  }
+  tags = {
+    Name = "basicApp-public-route-table"
+  }
+}
+
+# associate public subnet and route table
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+######################
+# private subnet
+######################
 
 # private subnet for web server
 resource "aws_subnet" "private_subnet_for_web" {
