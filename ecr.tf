c5c820b780ece03802bdd594802e0937bb9697f1
梅木崇浩 <forprogramming.b1@gmail.com>
2025-03-25 00:11:52 +0900

    create 2 repos for web and app

--- ecr.tf
+++ ecr.tf
@@ -1,9 +1,36 @@
-resource "aws_ecr_repository" "ecr" {
-  name = "basic-app-ecr"
+resource "aws_ecr_repository" "basic_app_web_ecr" {
+  name = "basic-app-web-ecr"
 }
 
-resource "aws_ecr_lifecycle_policy" "ecr_lifecycle_policy" {
-  repository = aws_ecr_repository.ecr.name
+resource "aws_ecr_lifecycle_policy" "web_ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.basic_app_web_ecr.name
+  policy     = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 10 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["release"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 10
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
+}
+
+resource "aws_ecr_repository" "basic_app_app_ecr" {
+  name = "basic-app-app-ecr"
+}
+
+resource "aws_ecr_lifecycle_policy" "app_ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.basic_app_app_ecr.name
   policy     = <<EOF
   {
     "rules": [
