ac303d6462ad004fd90d19d1d6262ff68d7eeeff
梅木崇浩 <forprogramming.b1@gmail.com>
2025-03-31 00:11:05 +0900

    cicd: task definition needs execution role for cicd (not ca)

--- ecs_service_web.tf
+++ ecs_service_web.tf
@@ -29,6 +29,27 @@ resource "aws_ecs_task_definition" "web_ecs_task_definition" {
   memory                   = 512
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions    = file("./web_container_definitions.json")
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
