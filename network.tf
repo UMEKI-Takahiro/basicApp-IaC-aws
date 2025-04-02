12b089986391c3cefcd4f4030c797c8009b85e1f
梅木崇浩 <forprogramming.b1@gmail.com>
2025-04-03 00:25:16 +0900

    Revert "cicd (not ca): create servers to test ecr connection"
    This reverts commit 4c34c8179b091b7d47f5914c52d864ccc1c9da83.

--- network.tf
+++ network.tf
@@ -164,14 +164,6 @@ resource "aws_security_group_rule" "sg_ingress_rule_https_for_web" {
   cidr_blocks       = ["10.0.0.0/16"]
   security_group_id = aws_security_group.sg_for_web.id
 }
-resource "aws_security_group_rule" "sg_ingress_rule_ssh_for_web" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["10.0.0.0/24"]
-  security_group_id = aws_security_group.sg_for_web.id
-}
 
 # private subnet for app server
 resource "aws_subnet" "private_subnet_for_app" {
